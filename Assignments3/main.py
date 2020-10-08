654c53b0b2dfc925b0da7c6962eb3f9a05c30dd5
MaximilianSchnatterer <m.schnatterer@student.utwente.nl>
2020-10-08 21:29:30 +0200

    sudoku

--- Assignments3/main.py
+++ Assignments3/main.py
@@ -1,3 +1,7 @@
+
+from sudoku_solver import SudokuSolver
+
+
 sudoku = [
 [5, 3, 0, 0, 7, 0, 0, 0, 0],
 [6, 0, 0, 1, 9, 5, 0, 0 ,0],
@@ -9,4 +13,6 @@ sudoku = [
 [0, 0, 0, 4, 1, 9, 0, 0, 5],
 [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
-print(sudoku)
+
+
+solver = SudokuSolver(sudoku)
--- Assignments3/sudoku_solver.py
+++ Assignments3/sudoku_solver.py
@@ -1,5 +1,11 @@
 
 
-class SudokuSolver:
+class SudokuSolver(object):
+
+    def __init__(self, sudoku):
+
+        self.sudoku = sudoku
+        self.is_solved = False
+        return
 
     
